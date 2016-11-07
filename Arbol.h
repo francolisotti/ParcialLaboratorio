4c4cd823bd9d33969986d439f58f9cdf29e7f132
Agustin San Martin <Elsanma@users.noreply.github.com>
2016-11-07 15:09:27 -0300

    Add files via upload

---
+++ Arbol.h
@@ -0,0 +1,19 @@
+#ifndef ARBOL_H_INCLUDED
+#define ARBOL_H_INCLUDED
+
+typedef struct
+{
+    persona p;
+    struct nodoArbol * izq;
+    struct nodoArbol * der;
+} nodoArbol;
+
+nodoArbol * inicArbol ();
+nodoArbol * crearNodoArbol (persona p);
+nodoArbol * insertarNodoArbol (nodoArbol * arbol, persona p);
+void mostrarArbol (nodoArbol * arbol, int orden);
+nodoArbol * nodoMasDerecho(nodoArbol * arbol);
+nodoArbol * nodoMasIzquierdo(nodoArbol * arbol);
+nodoArbol * borrarNodoArbol (nodoArbol * arbol, char nombre []);
+
+#endif // ARBOL_H_INCLUDED
---
+++ Persona.h
@@ -0,0 +1,22 @@
+#ifndef PERSONA_H_INCLUDED
+#define PERSONA_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include "string.h"
+
+typedef struct
+{
+    char nombreApellido [40];
+    int cantArticulos; // es el tiempo de ejecución
+    int tiempoDeEspera; // es el tiempo de respuesta
+    int tiempoProcesado; // es el tiempo que ya fue procesado en la línea de caja
+    int tipo_cliente; // prioridad (1: embarazada, 2: jubilado y 3: normal)
+    int tipo_pago; // 1 efectivo, 2 crédito o débito, 3 todos
+}persona;
+
+persona nuevo_Persona();
+persona crearPersona (char nombreApellido[], int cantArticulos,int tipo_Cliente,int tipo_Pago);
+void cargarPersona (char nombreDelArchivo[]);
+void mostrarPersona(persona aux);
+
+#endif // PERSONA_H_INCLUDED
